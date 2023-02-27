02ebd1f85d7ca58c93ac248bdb3898a31b09219c
Никитосик Дворницын <nikitadvornitsyn@MacBook-Pro-Nikitosik.local>
2023-02-27 10:10:50 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = '6080835241:AAGbIitOW7IdPmsaVEdWDXez61MPqL5OtA8'
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+
+    await message.reply('Nikita Dvornitsyn\'s test bot')
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=False)
+
+
+
