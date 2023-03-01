8ab3c3e9fbfbab3f73f654fd62f540d45cf31bb4
ma3str0o0 <nikitadvornitsyn@gmail.com>
2023-03-01 13:54:31 +0400

    working condition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types
+import requests
 
 API_TOKEN = '6080835241:AAGbIitOW7IdPmsaVEdWDXez61MPqL5OtA8'
 logging.basicConfig(level=logging.INFO)
@@ -20,8 +21,26 @@ async def echo(message: types.Message):
     await message.answer(message.text)
 
 
+@dp.message_handler(commands=['weather'])
+async def question_answer(message: types.Message):
+
+    cities = {
+        'Moscow',
+        'Dubai',
+        'New-York',
+        'Yekaterinburg',
+        'Abu-Dhabi',
+    }
+    url = 'https://wttr.in'
+    # не изменяйте значение URL
+
+    weather_parameters = {
+        '0': '',
+        'T': '',
+        'M': '',
+        'lang': 'ru'
+    }
+
+
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=False)
-
-
-
